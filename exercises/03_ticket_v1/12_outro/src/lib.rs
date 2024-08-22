16e825c6bd1b0f2ef92325ef3470f6a25866c611
Joachim Goris <joachim.goris@codit.eu>
2024-08-22 13:35:10 +0200

    wip outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -19,12 +19,44 @@ struct Order {
 
 impl Order {
     pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
         Self {
             product_name,
             quantity,
             unit_price
         }
     }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn product_name(&mut self, new_product_name: String){
+        Self::validate_product_name(&new_product_name);
+        self.product_name = new_product_name
+    }
+
+    pub fn quantity(&mut self, new_quantity: u32) {
+        Self::validate_quantity(&new_quantity);
+        self.quantity = new_quantity
+    }
+
+    pub fn unit_price(&mut self, new_unit_price: u32) {
+        Self::validate_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price
+    }
+
+    
 
     fn validate_product_name(product_name: &String) {
         if product_name.len() == 0 {
@@ -36,13 +68,13 @@ impl Order {
     }
 
     fn validate_quantity(quantity: &u32) {
-        if quantity == 0 {
+        if quantity == &0 {
             panic!("Quantity must be greater than 0")
         }
     }
 
     fn validate_unit_price(unit_price: &u32) {
-        if unit_price == 0 {
+        if unit_price == &0 {
             panic!("Unit price must be greater than 0")
         }
     }
