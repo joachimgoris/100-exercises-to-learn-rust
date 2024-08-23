1caa37ef68339ba4ac0e6199146aa0fb260fa9b5
Joachim Goris <joachim.goris@codit.eu>
2024-08-23 23:21:51 +0200

    wip

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -9,6 +9,14 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl core::ops::Add for WrappingU32 {
+    type Output = u32;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        
     }
 }
 
